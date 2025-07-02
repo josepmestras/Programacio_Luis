73434d2620f89636d0821b125681689525309cf7
josepmestras <josep.mestrasv@gmail.com>
2025-07-02 21:52:35 +0200

    cuentaLetras añadido a 'Ejercicios'

---
+++ Ejercicios/cuentaLetras.js
@@ -0,0 +1,11 @@
+let string = process.argv[2];
+let letra = process.argv[3];
+let result = 0;
+
+for (let i = 0; i <= string.length; i++) {
+    if (string[i] == letra) {
+        result++;
+    }
+}
+
+console.log(result);
