5c091f996b2fb09fd982896dd9e68859a60857cf
josepmestras <josep.mestrasv@gmail.com>
2025-07-03 21:26:07 +0200

    arraymax mejorado

---
+++ Ejercicios/arraymax.js
@@ -0,0 +1,25 @@
+function arraymax(items) {
+    let numActual = 0;
+
+    for(let i = 0; i <= items.length; i++) {
+        if (items[i] > numActual) {
+            numActual = items[i];
+
+            
+        }
+    }
+    
+    return numActual;
+}
+
+function pruebaArrayMax(items, esperado) {
+    let obtenido = arraymax(items);
+    
+    if (obtenido != esperado) {
+        console.error(`Valor esperado: ${esperado}, valor obtenido: ${obtenido}`);
+    } else {
+        console.log(obtenido);
+    }
+} 
+
+pruebaArrayMax([7, 2, 9, 4, 1], 21);
